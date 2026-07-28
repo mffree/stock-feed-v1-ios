15390633c09122ef04d05ea641fee18f4a858847
Matthew Freeman <matthewfreeman63@gmail.com>
2026-07-28 12:52:49 -0400

    Initial Xcode project

---
+++ Stock Feed v1 ios/Stock Feed v1 ios/Stock_Feed_v1_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stock_Feed_v1_iosApp.swift
+//  Stock Feed v1 ios
+//
+//  Created by Matthew Freeman on 7/28/26.
+//
+
+import SwiftUI
+
+@main
+struct Stock_Feed_v1_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
